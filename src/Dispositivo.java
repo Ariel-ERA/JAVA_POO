a0596c4945faaf0a885b21c55d477d907e086424
Juan Aedo <j.aedo@duocuc.cl>
2026-09-25 16:04:42 -0300

    Trabajo Ariel

--- src/Dispositivo.java
+++ src/Dispositivo.java
@@ -2,15 +2,13 @@
 public class Dispositivo {
 
     private String nombre;
-    private boolean activo; ;
+    private boolean activo;
 
-    public Dispositivo(){
-    }
-    public Dispositivo(String nombre , boolean activo) {
+    public Dispositivo(String nombre, boolean activo) {
         this.nombre = nombre;
         this.activo = activo;
     }
-    public void mostrarEstatus(){
+    public void mostrarEstatus() {
         System.out.println(nombre + " - " + activo);
     }
     public void ejecutarDiagnostico() {
